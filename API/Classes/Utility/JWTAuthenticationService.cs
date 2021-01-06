88d7fb2d95392faebabf36c6fe6cd6cc1be4c8f1
peter bou saada <peterbousaada@gmail.com>
2021-01-05 19:09:06 -0500

    Added structure for JWT Authentication

---
+++ API/Classes/Utility/JWTAuthenticationService.cs
@@ -0,0 +1,17 @@
+﻿using API.Interfaces;
+using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Classes.Utility
+{
+    public class JWTAuthenticationService : IJWTAuthenticationService
+    {
+        public string CreateJWTToken(User user)
+        {
+            return null;
+        }
+    }
+}
